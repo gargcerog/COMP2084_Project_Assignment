4d3af6d36fd2d3db0e68717356b69efee691ffe2
Subjective <gargcero@protonmail.com>
2022-06-14 22:51:50 -0400

    Added more fields to Deparment Class

--- COMP2084_Project_Assignment/Models/Department.cs
+++ COMP2084_Project_Assignment/Models/Department.cs
@@ -1,13 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace COMP2084_Project_Assignment.Models
 {
     public class Department
     {
-        [Key]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100), MinLength(1)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(200), MinLength(1)]
+        public string Address { get; set; }
+        [Required]
+        [MaxLength(6), MinLength(6)]
+        public string PostalCode { get; set; }
         public ICollection<Employee>? Employees { get; set;}
     }
 }
