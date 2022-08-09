55cd33ed2a8deb19af66aff8e9c638cfdb95ed46
Guillermo Alvarez <38199326+gargcerog@users.noreply.github.com>
2022-08-08 22:15:46 -0400

    Installed nuget packages and added dependecies to the test class

--- COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
+++ COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
@@ -1,4 +1,11 @@
-﻿namespace COMP2084_Project_Assignment.Tests
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2084_Project_Assignment.Controllers;
+using COMP2084_Project_Assignment.Data;
+using COMP2084_Project_Assignment.Models;
+
+namespace COMP2084_Project_Assignment.Tests
 {
     public class EmployeesControllerTest
     {
