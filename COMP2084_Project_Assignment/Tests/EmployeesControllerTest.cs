39598d7c9b01e6ebc78c6270e79dc19a9faa39d1
Guillermo Alvarez <38199326+gargcerog@users.noreply.github.com>
2022-08-09 18:25:27 -0400

    Added method for creating fake employees

--- COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
+++ COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
@@ -11,11 +11,28 @@ namespace COMP2084_Project_Assignment.Tests
     public class EmployeesControllerTest
     {
 
+        // Method returns a DbContextOptions<ApplicationDbContext> object with out in memory database configurations for running the tests
         private DbContextOptions<ApplicationDbContext> GetTestDbOpts() 
         {
             return new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "TestEmployeesCtrllr").Options;
-            
         }
 
+        // Method generates fake employees for testing
+        protected List<Employee> MakeFakeEmployees(int n)
+        {
+            Random random = new Random();
+            var Employees = new List<Employee>();
+            for (int i = 0; i < n; i++)
+            {
+                Employees.Add(new Employee
+                {
+                    Name = $"testName{i}",
+                    UserName = $"testUserName{i}",
+                    PhoneNumber = $"testPhone{i}",
+                    Age = random.Next(20,60)
+                });
+            }
+            return Employees;
+        }
     }
 }
