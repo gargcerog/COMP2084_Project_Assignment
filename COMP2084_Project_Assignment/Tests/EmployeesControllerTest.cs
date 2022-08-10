dc7ffef4e9ed336078ccdfb2a432510d0bfda4d2
Guillermo Alvarez <38199326+gargcerog@users.noreply.github.com>
2022-08-09 21:23:08 -0400

    Created a Test for the method Create POST form the Employees Controller

--- COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
+++ COMP2084_Project_Assignment/Tests/EmployeesControllerTest.cs
@@ -4,12 +4,33 @@ using Microsoft.EntityFrameworkCore;
 using COMP2084_Project_Assignment.Controllers;
 using COMP2084_Project_Assignment.Data;
 using COMP2084_Project_Assignment.Models;
-
+using System.Diagnostics;
 
 namespace COMP2084_Project_Assignment.Tests
 {
     public class EmployeesControllerTest
     {
+
+        [Fact]
+        public async Task Create_Employees_Test()
+        {
+            using (var testDb = new ApplicationDbContext(GetTestDbOpts()))
+            {
+                var testCtrl = new EmployeesController(testDb);
+                var fakeEmployees = MakeFakeEmployees(3);
+
+                // Adding new Employees
+                foreach (var employee in fakeEmployees)
+                {
+                    var result = await testCtrl.Create(employee);
+                    var resultVr = Assert.IsType<RedirectToActionResult>(result);
+                    Assert.Equal("Index", resultVr.ActionName);
+                }
+            }
+        }
+
+
+        //----------------- Additional Methods---------------------------
 
         // Method returns a DbContextOptions<ApplicationDbContext> object with out in memory database configurations for running the tests
         private DbContextOptions<ApplicationDbContext> GetTestDbOpts() 
